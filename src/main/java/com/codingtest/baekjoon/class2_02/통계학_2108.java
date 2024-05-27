8d35bf719d2d214eee647dd7e30f29877d5739b9
JangSuhyeon <shjang0043@gmail.com>
2024-05-27 15:29:06 +0900

    5/27

--- src/main/java/com/codingtest/baekjoon/class2_02/통계학_2108.java
+++ src/main/java/com/codingtest/baekjoon/class2_02/통계학_2108.java
@@ -3,9 +3,7 @@ package com.codingtest.baekjoon.class2_02;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class 통계학_2108 {
     public void solution() throws NumberFormatException, IOException {
@@ -14,48 +12,40 @@ public class 통계학_2108 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        // 1. 배열 저장 및 최빈값 찾기
+        // 1. 배열 저장, 산술평균
+        int[] arr = new int[N];
         double sum = 0;
-        int[][] arr = new int[N][2]; // {수, 카운트}
+        int[] cnt = new int[8001];
         for (int i = 0; i < N; i++) {
-            arr[Integer.parseInt(br.readLine())][0] = Integer.parseInt(br.readLine());
-            arr[Integer.parseInt(br.readLine())][1]++;
-            sum += arr[i][0];
+            int now = Integer.parseInt(br.readLine());
+            arr[i] = now;
+            sum += now;
+            cnt[now + 4000]++;
         }
-
-        // 2. 산술평균
         System.out.println(Math.round(sum / N));
 
-        // 3. 수의 크기를 기준으로 오름차순 정렬
-        Arrays.sort(arr, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[0] - o2[0];
+        // 2. 중앙값
+        Arrays.sort(arr);
+        System.out.println(arr[N / 2]);
+
+        // 3. 최빈값
+        int idx = 0;
+        int max = 0;
+        boolean flag = false;
+        for (int i = 0; i < cnt.length; i++) {
+            if (cnt[i] > max) {
+                max = cnt[i];
+                idx = i - 4000;
+                flag = true;
+            } else if (cnt[i] == max && flag) {
+                idx = i - 4000;
+                flag = false;
             }
-        });
-
-        // 4. 중앙값
-        System.out.println(arr[N / 2][0]);
-
-        // 5. 범위
-        int range = arr[N - 1][0] - arr[0][0];
-
-        // 6. 빈도수를 기준으로 오름차순 정렬
-        Arrays.sort(arr, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                if (o1[1] == o2[1]) return o2[0] - o1[0];
-                return o1[1] - o2[1];
-            }
-        });
-
-        for (int[] ints : arr) {
-            System.out.println(ints[0] + " " + ints[1]);
         }
-        // 8. 최빈값
-        System.out.println(arr.length == 1 ? arr[0][0] : arr[N - 1][1] == arr[N - 2][1] ? arr[N - 2][0] : arr[N - 1][0]);
+        System.out.println(idx);
 
-        // 7. 범위 출력
+        // 4. 범위 출력
+        int range = arr[N - 1] - arr[0];
         System.out.println(range);
     }
 }
